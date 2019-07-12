5c08ee2144d5b0d6d0d0a260a61a9a5e05c7e8df
marieval <majiku@seznam.cz>
2019-07-12 16:19:23 +0200

    adding all my new blog code for gatsby setup

---
+++ src/markdown-pages/first-thoughts.md
@@ -0,0 +1,14 @@
+---
+title: "Hi, I´m GatsbyJS"
+date: "2019-07-12"
+---
+
+### I´m GatsbyJS --- Nice to meet you.
+
+I just heard about GatsbyJS earlier, but never used it. Now I learned it´s basics and created this sample blog when learning React in the course from Andrei Neagoie on Udemy [The Complete React Developer Course](https://www.udemy.com/react-2nd-edition/). 
+
+> I find GatsbyJS interesting and have already some ideas where I could use it: 
+
+  > Perhaps I will create a website with my favourite dishes. 
+
+  > My second thought about using GatsbyJS is log of the places which I recommend for spending holiday. 
---
+++ src/pages/index.js
@@ -0,0 +1,61 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+  `
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`  
+
+export default ({ data }) => {
+console.log(data);
+return (
+  <Layout>
+    <SEO title="Home" />
+    <div>
+      <h1>My Thoughts about GatsbyJS </h1>
+      <h4>{ data.allMarkdownRemark.totalCount } posts</h4>
+      {data.allMarkdownRemark.edges.map(({node}) => (
+          <div key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                { node.frontmatter.title } ({ node.frontmatter.date })
+              </BlogTitle>
+            </BlogLink>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+    </div>
+  </Layout>
+)
+} 
+
+export const query = graphql`
+query {
+  allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    totalCount
+    edges {
+      node {
+        frontmatter {
+          date
+          description
+          title
+        }
+        fields {
+          slug
+        }
+        excerpt
+        id
+      }
+    }
+  }
+}
+`
